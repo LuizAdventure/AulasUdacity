1e2cc0ec4940dfb563cabdee1f23e4fb9fc0f850
LuizAdventure <luizsbb@gmail.com>
2022-01-18 09:41:32 -0300

    refatoração:inicializando variavel imagem

--- app/src/main/java/com/example/niceroller/MainActivity.kt
+++ app/src/main/java/com/example/niceroller/MainActivity.kt
@@ -1,32 +1,29 @@
 package com.example.niceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import java.lang.Math.random
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView mostra o layout a ser referenciado
         setContentView(R.layout.activity_main)
-        // roolButton nome dado a variavel
-        // Button tipo da variavel
-        // findViewById acha o id referenciado
+
         val rollButton: Button = findViewById(R.id.roll_button)
 
-        // setOnclickListener define o click a ser ouvido
         rollButton.setOnClickListener {
-            // exibe uma mensagem, na qual foi definida
             Toast.makeText(this,"Você clicou no botão", Toast.LENGTH_SHORT).show()
-            // chamando a função
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
+
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt){
@@ -37,8 +34,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_6
             else -> R.drawable.dice_6
         }
-
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
 
 
